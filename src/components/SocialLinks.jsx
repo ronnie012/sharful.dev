372c975ef93b0cb943d6fcd45d5afd09d6c1a3c6
ronnie012 <ronnie.247365.1@gmail.com>
2025-12-11 10:51:11 +0600

    Updated social link.

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -4,7 +4,7 @@ import { FaFacebook, FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
 const SocialLinks = () => {
   return (
     <div className="flex space-x-4 justify-center">
-      <a href="https://www.linkedin.com/in/sharful-islam/" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-white transition-colors duration-300">
+      <a href="https://www.linkedin.com/in/md-sharful-islam/" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-white transition-colors duration-300">
         <FaLinkedin className="text-2xl" />
       </a>
       <a href="https://github.com/ronnie012" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-white transition-colors duration-300">
